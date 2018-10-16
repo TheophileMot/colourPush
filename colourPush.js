a4c6881851efeec26a16235416f8ce8c2d91ec0b
Timothy Mott <tjbmott+github@gmail.com>
2018-10-16 15:06:34 -0400

    animation on colour cube

--- colourPush.js
+++ colourPush.js
@@ -1,5 +1,5 @@
 const EPSILON = 1;
-const PUSH_MULTIPLIER = 1e2;
+const PUSH_MULTIPLIER = 1e4;
 
 class Colour {
 
@@ -84,20 +84,19 @@ function setUpColourScheme() {
     new Colour(255, 255, 255, true),
   ];
   let displayColours = [
-    // new Colour(  0,   0,   0, true),
-    // new Colour(128, 97, 84),
-    // new Colour(134, 169, 103),
-    // new Colour(196, 179, 126),
-    // new Colour(115, 122, 114),
-    // new Colour(214, 222, 209),
+    new Colour(128, 97, 84),
+    new Colour(134, 169, 103),
+    new Colour(196, 179, 126),
+    new Colour(115, 122, 114),
+    new Colour(214, 222, 209),
   ];
 
-  for (let i = 0; i < 24; i++) {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    displayColours.push(new Colour(r, g, b));
-  }
+  // for (let i = 0; i < 24; i++) {
+  //   let r = Math.floor(Math.random() * 256);
+  //   let g = Math.floor(Math.random() * 256);
+  //   let b = Math.floor(Math.random() * 256);
+  //   displayColours.push(new Colour(r, g, b));
+  // }
 
   for (let i = 0; i < displayColours.length; i++) {
     let colour = displayColours[i];
@@ -120,16 +119,17 @@ function setUpColourScheme() {
 }
 
 function pushLoop(colourScheme) {
+  if (!$('#button-push').hasClass('active')) { return; }
+
   pushColours(colourScheme);
   for (let i = 0; i < colourScheme.displayColours.length; i++) {
     $(`#colour-box-current-${i}`).css('background-color', colourScheme.displayColours[i].rgb);
     $(`#colour-box-initial-${i}`).css('background-color', colourScheme.displayColours[i].initialRgb);
   }
+  updateCanvas(colourScheme);
 }
 
 function pushColours({ avoidColours, displayColours }) {
-  if (!$('#button-push').hasClass('active')) { return; }
-
   for (let i = 0; i < displayColours.length; i++) {
     for (let j = 0; j < displayColours.length; j++) {
       if (i !== j) {
@@ -150,6 +150,66 @@ function pushColours({ avoidColours, displayColours }) {
   }
 
   displayColours.sort((colA, colB) => colA.luminance - colB.luminance);
+}
+
+function updateCanvas(colourScheme) {
+  let canvas = document.getElementById('push-map');
+  let ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#aaa';
+  ctx.fillRect(1, 1, 400, 240);
+  
+  // Sort by depth of 3d display.
+  let allColours = [...colourScheme.avoidColours, ...colourScheme.displayColours];
+  allColours.sort((colA, colB) => (colA.r - colA.g) - (colB.r - colB.g));
+  for (let colour of allColours) {
+    drawPoint3d(ctx, colour);
+  }
+}
+
+function drawPoint3d(ctx, colour) {
+  let { r, g, b } = colour;
+  // Scale to [-50, 50].
+  let scaledR = (r / 255 - 0.5) * 100;
+  let scaledG = (g / 255 - 0.5) * 100;
+  let scaledB = (b / 255 - 0.5) * 100;
+  let x = 200 + scaledR + scaledG;
+  let y = 120 + 0.5 * scaledR - 0.5 * scaledG - scaledB;
+
+  let borderColour = colour.luminance < 0.5 ? '#fff' : '#000';  
+  if (colour.fixed ) {
+    drawDisc3d(ctx, x, y, colour, 8 + 5 * (r - g) / 255, { borderColour });
+  } else {
+    drawSquare(ctx, x, y, colour, 8 + 5 * (r - g) / 255, borderColour);
+  }
+}
+
+function drawDisc3d(ctx, x, y, colour, size, { borderColour = '#000', stroke = true }) {
+  if (borderColour && !stroke) {
+    ctx.beginPath();
+    ctx.fillStyle = borderColour;
+    ctx.arc(x, y, size + 1, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  ctx.beginPath();
+  ctx.fillStyle = colour.rgb;
+  ctx.strokeStyle = colour.rgb;
+  ctx.lineWidth = size / 5;
+  ctx.arc(x, y, size, 0, 2 * Math.PI);
+  stroke ? ctx.stroke() : ctx.fill();
+}
+
+function drawSquare(ctx, x, y, colour, size, borderColour = '#000') {
+  if (borderColour) {
+    ctx.fillStyle = borderColour;
+    ctx.fillRect(x - size, y - size, 2 * size + 1, 2 * size + 1);
+  }
+  ctx.fillStyle = colour.rgb;
+  ctx.fillRect(x + 1 - size, y + 1 - size, 2 * size - 1, 2 * size - 1);
+}
+
+function rgbAlpha(rgb, alpha) {
+  return rgb.replace(/rgb\((.*)\)/, `rgba($1, ${alpha})`);
 }
 
 function clamp(x, min, max) {
